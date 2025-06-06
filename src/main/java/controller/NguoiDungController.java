6ac2b8c3cd8546b6bd191138a8e330942aba5c9c
VietNamCombatZ <nguyenduchuy2005hnh@gmail.com>
2025-06-07 00:24:49 +0700

    xoá file dư

--- src/main/java/controller/NguoiDungController.java
+++ src/main/java/controller/NguoiDungController.java
@@ -537,7 +537,7 @@ public class NguoiDungController  extends BaseController {
             return;
         }
 
-//        req.getRequestDispatcher("/user.jsp").forward(req, res);
+
         render(req, res, "thongTinNguoiDung");
     }
 
@@ -643,37 +643,7 @@ public class NguoiDungController  extends BaseController {
 
     }
 
-//    @WebServlet("/CapNhatThongTin")
-//    public class CapNhatThongTin extends HttpServlet {
-//        protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//            nguoiDung nd = (nguoiDung) request.getSession().getAttribute("nguoiDung");
-//            if (nd == null) {
-//                response.sendRedirect("dangNhap.jsp");
-//                return;
-//            }
-//
-//            request.setCharacterEncoding("UTF-8");
 
-//            nd.setTen(request.getParameter("ten"));
-//            nd.setEmail(request.getParameter("email"));
-//            nd.setAnhDaiDien(request.getParameter("anhDaiDien"));
-//
-//            try {
-//                String ngaySinhStr = request.getParameter("ngaySinh");
-//                if (ngaySinhStr != null && !ngaySinhStr.isEmpty()) {
-//                    Date ngaySinh = new SimpleDateFormat("yyyy-MM-dd").parse(ngaySinhStr);
-//                    nd.setNgaySinh(ngaySinh);
-//                }
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//
-//
-//            NguoiDungDAO.capNhat(nd);
-//            request.getSession().setAttribute("nguoiDung", nd); // cập nhật lại session
-//            response.sendRedirect("user.jsp"); // quay lại trang cá nhân
-//        }
-//    }
 
     private void layDanhSachKhachHang(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         nguoiDung nd = (nguoiDung) request.getSession().getAttribute("nguoiDung");
